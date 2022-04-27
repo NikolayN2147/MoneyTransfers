c526ec79e3f470c185620681ea2874bef0886f55
NikolayN2147 <100046842+NikolayN2147@users.noreply.github.com>
2022-04-27 22:19:24 +0300

    rework

--- src/main/kotlin/MoneyTransfer.kt
+++ src/main/kotlin/MoneyTransfer.kt
@@ -1,11 +1,7 @@
+
 fun main() {
-    print("Введите сумму: ")
-    val amount = readLine()?.toUInt() ?: return
-    if(amount > 35U){
-        val result = amount * 75u / 100u
-        println("Комиссия при переводе $amount рублей составит " + (result / 100u) + " рублей "
-              + (result % 100u) + " копеек")
-    }else{
-        println("Ошибка.Минимальная сумма перевода должна быть 35 рублей")
-    }
+    val amount = 11000L
+    var comission = amount * 0.0075
+    if (comission < 35) comission = 35.0 else comission
+    println("Комиссия за перевод: " + comission.toInt() + " руб. " + ((comission * 100) % 100).toInt() + " коп." )
 }
